0ddc960518862e3741d727e3d238621ed87dd136
Bardia Moshiri <fakeshell@bardia.tech>
2023-01-09 18:46:34 -0500

    fix EOF issue

--- scripts/install-adaptation.sh
+++ scripts/install-adaptation.sh
@@ -51,7 +51,7 @@ curl https://mirror.bardia.tech/exynos9810/exynos9810.gpg | sudo apt-key add -
 curl -sS -o /etc/apt/sources.list.d/exynos9810.list https://mirror.bardia.tech/exynos9810/exynos9810.list
 apt update
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="->
+DEBIAN_FRONTEND=noninteractive apt install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y adaptation-droidian-exynos9810 adaptation-exynos9810-configs
 systemctl disable systemd-resolved systemd-timesyncd upower bluetooth
 systemctl mask systemd-resolved systemd-timesyncd upower bluetooth
 
